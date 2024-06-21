12f75747386443dc72f44142f6e2fd7c541a539c
seelmacheg <chegdalisalma@gmail.com>
2024-06-21 01:18:26 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: use the write function to print a text
+ *
+ * Return: Always 1 (Success)
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (1);
+}
