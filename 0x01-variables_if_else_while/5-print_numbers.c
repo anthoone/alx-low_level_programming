a49942409ee7b73559d56be7c28bfa38c828e856
anthoone <david.anthone49@gmail.com>
2024-06-23 06:22:58 +0100

    5-print_numbers commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char num;
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+
+	return (0);
+}
