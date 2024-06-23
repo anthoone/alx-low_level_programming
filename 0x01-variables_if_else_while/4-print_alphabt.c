7072e3b2cc19406abda113cafdc6a819f97055f8
anthoone <david.anthone49@gmail.com>
2024-06-23 06:07:14 +0100

    4-print_alphabt commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet in lowercase except q and e
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (!(c == 'q' || c == 'e'))
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
