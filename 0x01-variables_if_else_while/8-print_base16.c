fe0039bd1d84f7bb6380065fc88aa5a00c874762
anthoone <david.anthone49@gmail.com>
2024-06-23 06:58:52 +0100

    8-print_base16 commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+	}
+	for (c = 97; c <= 102; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
