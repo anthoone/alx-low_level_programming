e728b10cb3759f0a893d7e2c10acc12d82866fc3
anthoone <david.anthone49@gmail.com>
2024-06-25 23:17:58 +0100

    10-add commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - dds two integers
+ *
+ * @a: first integer
+ * @b : seconde integer
+ *
+ * Description: create a function that adds two integers and returns the result
+ *
+ * Return : the result of a + b
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
