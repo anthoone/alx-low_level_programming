c01d975f76c84fdaff77cb64c471b8ce2d6d0418
anthoone <david.anthone49@gmail.com>
2024-06-25 21:20:14 +0100

    9-times_table commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Description: prints the 9 times table, starting with 0.
+ *
+ * Return: nothing
+*/
+
+void times_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(48);
+		for (mult = 0; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+			_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
