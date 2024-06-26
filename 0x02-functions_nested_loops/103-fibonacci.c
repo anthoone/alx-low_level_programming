ef16dcf359247d2b257e0ab7879e0c598f072b9c
anthoone <david.anthone49@gmail.com>
2024-06-26 03:41:50 +0100

    103-fibonacci commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: finds and prints the sum of the even-valued terms
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	unsigned long first = 1, second = 2, sum = 2, next;
+
+	while (1)
+	{
+		next = first + second;
+		if (next > 4000000)
+			break;
+		if (next % 2 == 0)
+			sum += next;
+
+		first = second;
+		second = next;
+	}
+	printf("%lu\n", sum);
+
+	return (0);
+}
