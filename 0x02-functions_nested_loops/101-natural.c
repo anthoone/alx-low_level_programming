178a049e060ef86c2a7eed410086d45cece257c1
anthoone <david.anthone49@gmail.com>
2024-06-26 02:17:08 +0100

    101-natural commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  computes and prints the sum of all the
+ *	multiples of 3 or 5 below 1024
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 3; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
