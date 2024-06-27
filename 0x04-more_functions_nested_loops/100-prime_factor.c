6baf19669e8b069fbe9ebd103e101c660e824a58
anthoone <david.anthone49@gmail.com>
2024-06-27 23:32:00 +0100

    100-prime_factor commit

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: finds and prints the largest prime factor of a number
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	unsigned long long count, largest_prime, num = 612852475143;
+
+	while (num % 2 == 0)
+	{
+		largest_prime = 2;
+		num /= 2;
+	}
+	for (count = 3; count * count <= num; count += 2)
+	{
+		while (num % count == 0)
+		{
+			largest_prime = count;
+			num /= count;
+		}
+	}
+
+	if (num > 2)
+		largest_prime = num;
+
+	printf("%llu\n", largest_prime);
+
+	return (0);
+}
