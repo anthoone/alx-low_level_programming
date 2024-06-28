1aee104ca7897ad2e7069bedbccdba0849c3ac05
anthoone <david.anthone49@gmail.com>
2024-06-28 05:30:02 +0100

    last commit for 0x04 project

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,14 +12,16 @@
 
 void print_number(int n)
 {
+	unsigned int num = n;
+
 	if (n < 0)
 	{
-		n *= -1;
+		num *= -1;
 		_putchar('-');
 	}
 
-	if ((n / 10) > 0)
-		print_number(n / 10);
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	_putchar((n % 10) + 48);
+	_putchar((num % 10) + 48);
 }
