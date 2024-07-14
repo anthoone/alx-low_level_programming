0fb4dbbb7abefa98adf68529d47307d9e076ff11
anthoone <david.anthone49@gmail.com>
2024-07-14 18:34:20 +0100

    6-puts2 commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @str: input string
+ *
+ * Description: function that prints every other character of a string
+*/
+
+void puts2(char *str)
+{
+	char *P;
+
+	P = str;
+	while (*P)
+	{
+		_putchar(*P);
+		P += 2;
+	}
+	_putchar('\n');
+}
