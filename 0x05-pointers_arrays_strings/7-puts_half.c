b2fdabd9bf0bd015e910339271323a4da3bc675d
anthoone <david.anthone49@gmail.com>
2024-07-15 12:19:59 +0100

    7-puts_half commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: input string
+ *
+ * Description: function that prints half of a string
+*/
+
+void puts_half(char *str)
+{
+	char *c;
+	int n, len = 0;
+
+	for (c = str; *c != '\0'; c++)
+	{
+		len++;
+	}
+
+	if (len % 2 != 0)
+		n = (len - 1) / 2;
+	n = len / 2;
+
+	c = str + n;
+	while (*c != '\0')
+	{
+		_putchar(*c);
+		c++;
+	}
+	_putchar('\n');
+}
