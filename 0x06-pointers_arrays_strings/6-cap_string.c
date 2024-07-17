72e0df802bfb927ef1f60031fada17e4ae0f869e
anthoone <david.anthone49@gmail.com>
2024-07-17 23:14:54 +0100

    6-cap_string commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ *
+ * @str: input string
+ *
+ * Return: the capitalized str
+*/
+
+char *cap_string(char *str)
+{
+	int j, i = 0;
+	char *sep = ",;.?!\"(){} \n\t";
+
+	while (str[i++])
+	{
+		for (j = 0; sep[j]; j++)
+		{
+			if (str[i] == sep[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+					str[i + 1] -= 32;
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
