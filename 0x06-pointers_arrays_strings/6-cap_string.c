d7cf160875693caa3444d42d8ae34974f5ec145d
anthoone <david.anthone49@gmail.com>
2024-07-17 23:48:56 +0100

    6-cap_string commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,19 +13,20 @@ char *cap_string(char *str)
 	int j, i = 0;
 	char *sep = ",;.?!\"(){} \n\t";
 
-	while (str[i++])
+	while (str[i] != '\0')
 	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+
 		for (j = 0; sep[j]; j++)
 		{
-			if (str[i] == sep[j])
+			if (str[i] == sep[j] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
 			{
-				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-				{
-					str[i + 1] -= 32;
-					break;
-				}
+				str[i + 1] -= 32;
+				break;
 			}
 		}
+		i++;
 	}
 
 	return (str);
