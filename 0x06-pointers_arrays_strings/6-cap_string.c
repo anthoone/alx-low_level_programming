c5014232490099517a4407224c949dee8ffcfa52
anthoone <david.anthone49@gmail.com>
2024-07-17 23:22:33 +0100

    6-cap_string commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,8 +20,10 @@ char *cap_string(char *str)
 			if (str[i] == sep[j])
 			{
 				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
 					str[i + 1] -= 32;
-				break;
+					break;
+				}
 			}
 		}
 	}
